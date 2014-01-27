38e5862fec4a1e0058ed39fd0c3cdec6a69cde1c
alexdgarland <alex.d.garland@gmail.com>
2014-01-27 23:01:42 +0000

    Split "add_entab_blanks..." function out

--- Ch1/Lines.c
+++ Ch1/Lines.c
@@ -4,18 +4,18 @@
 
 int getline(char outputString[], int arrayLimit)
 {
-        int c, i;
+    int c, i;
         
-        for (i=0; i < (arrayLimit-1) && (c=getchar()) != EOF && c!='\n'; i++)
-        {
-            outputString[i] = c;
-        }
-        if (c == '\n')
-        {
-                outputString[i++] = c;
-        }
-        outputString[i] = '\0';
-        return i;
+    for (i=0; i < (arrayLimit-1) && (c=getchar()) != EOF && c!='\n'; i++)
+    {
+        outputString[i] = c;
+    }
+    if (c == '\n')
+    {
+            outputString[i++] = c;
+    }
+    outputString[i] = '\0';
+    return i;
 }
 
 void init_string(char s[], int arrayLimit)
@@ -67,9 +67,8 @@ int tabstospaces(char in_line[], int arrayLimit, char out_line[], int maxlength,
 
 int spacestotabs(char in_line[], int arrayLimit, char out_line[], int maxlength, short tabsize)
 {
-
 	char c;
-	int in_idx, out_idx = 0, i, blankstohandle = 0, spacestoadd, tabstoadd, blankstonexttab, idx_lastnonblank = 0;
+	int in_idx, out_idx = 0, blankstohandle = 0;
 	
 	for(in_idx = 0; ((in_idx < arrayLimit) && (out_idx < (maxlength - 1))); in_idx++)
 	{
@@ -77,44 +76,44 @@ int spacestotabs(char in_line[], int arrayLimit, char out_line[], int maxlength,
 
 		// If run of blanks or the line as a whole ends, output stored blanks in correct format
 		if(blankstohandle > 0 && ((c != '\t' && c != ' ') || (in_idx == (arrayLimit - 1))))
-		{			
-			// Handle (possibly) odd number of columns in first tab
-			blankstonexttab = tabsize - ((idx_lastnonblank + 1) % tabsize);
-			if(blankstohandle >= blankstonexttab)
-			{
-				out_line[out_idx++] = '\t';
-				blankstohandle -= blankstonexttab;
-			}
-		
-			// Deal with rest of required blanks
-			spacestoadd = blankstohandle % tabsize;
-			tabstoadd = blankstohandle / tabsize;
-			for(i = 0; i < tabstoadd; i++) { out_line[out_idx++] = '\t'; }
-			for(i = 0; i < spacestoadd; i++) { out_line[out_idx++] = ' '; }
-			
-			// Reset counter
-			blankstohandle = 0;
+		{
+			// Process optimised blanks into line and set index to new length of line (i.e. next write position)
+			out_idx = add_entab_blanks_to_string(out_line, out_idx, tabsize, blankstohandle);			
+			blankstohandle = 0;		// Reset counter
 		}
 
 		// If tab or space, store count of blanks. Otherwise, pass directly to output.
-		if (c == '\t')
+		switch(c)
 		{
-			blankstohandle += (tabsize - (in_idx % tabsize));
+			case '\t':
+				blankstohandle += (tabsize - (in_idx % tabsize));
+				break;
+			case ' ':
+				blankstohandle++;
+				break;
+			default:
+				out_line[out_idx++] = c;
 		}
-		else if (c == ' ')
-		{		
-			blankstohandle++;
-		}
-		else
-		{
-			idx_lastnonblank = out_idx;
-			out_line[out_idx++] = c;
-		}
-	
+
 	}
 
-	out_line[out_idx] = '\0';
-	
-	return out_idx;	// return length of output string
+	out_line[out_idx] = '\0';	
+	return out_idx;				// return length of output string
+}
 
+int add_entab_blanks_to_string(char line[], int idx, int tabsize, int numberofblanks)
+{
+	int blankstonexttab, i;
+
+	// Handle (possibly) odd number of columns in first tab
+	if(numberofblanks >= (blankstonexttab = tabsize - (idx % tabsize)))
+	{
+		line[idx++] = '\t';
+		numberofblanks -= blankstonexttab;
+	}
+	// Deal with rest of required blanks
+	for(i = 0; i < (numberofblanks / tabsize); i++) { line[idx++] = '\t'; }
+	for(i = 0; i < (numberofblanks % tabsize); i++) { line[idx++] = ' '; }
+
+	return idx;		// Return new length of string
 }
--- Ch1/Lines.h
+++ Ch1/Lines.h
@@ -3,3 +3,4 @@ int init_string(char s[], int arrayLimit);
 int trimline(char line[], int arrayLength);
 int tabstospaces(char in_line[], int arrayLimit, char out_line[], int maxlength, short tabsize);
 int spacestotabs(char in_line[], int arrayLimit, char out_line[], int maxlength, short tabsize);
+int add_entab_blanks_to_string(char line[], int idx, int tabsize, int numberofblanks);
